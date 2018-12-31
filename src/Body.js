38b7588b94a75287b2aa43ef7ef7708fb299b23c
Brendan Shea <bjrshea@gmail.com>
2018-12-30 19:51:49 -0800

    Add animations to hot air balloons.

--- src/Body.js
+++ src/Body.js
@@ -13,22 +13,39 @@ function Body() {
           z-index: -1;
         }
         .cloud:nth-child(1) {
+          top: 5%;
           opacity: 0.4;
           width: 120px;
           height: 70px;
           animation: blow 100s linear infinite;
         }
         .cloud:nth-child(2) {
+          top: 8%;
           opacity: 0.6;
           width: 140px;
           height: 80px;
           animation: blow 90s linear infinite reverse;
         }
         .cloud:nth-child(3) {
-          opacity: 0.8;
+          top: 10%;
+          opacity: 0.7;
           width: 180px;
           height: 100px;
-          animation: blow 75s linear infinite;
+          animation: blow 70s linear reverse;
+        }
+        .cloud:nth-child(4) {
+          top: 3%;
+          opacity: 0.8;
+          width: 140px;
+          height: 80px;
+          animation: blow 120s linear infinite;
+        }
+        .cloud:nth-child(5) {
+          top: 12%;
+          opacity: 0.5;
+          width: 160px;
+          height: 90px;
+          animation: blow 80s linear infinite;
         }
         @keyframes blow {
           from{left: -180px}
@@ -44,12 +61,32 @@ function Body() {
           width: 100%;
           height: 100%;
         }
+        .balloons {
+          display: flex;
+          justify-content: space-between;
+        }
         .balloon {
+          z-index: -2;
+          position: absolute;
           width: 45px;
           height: 67px;
         }
+        .balloon:nth-child(1) {
+          animation: fly 60s linear infinite;
+          left: 10%;
+        }
+        .balloon:nth-child(2) {
+          animation: fly 40s linear infinite;
+          right: 30%;
+        }
+        @keyframes fly {
+          from{bottom: -40px}
+          to{bottom: 100%}
+        }
       `}</style>
       <div className="clouds">
+        <img className="cloud" src={cloud} alt="cloud"/>
+        <img className="cloud" src={cloud} alt="cloud"/>
         <img className="cloud" src={cloud} alt="cloud"/>
         <img className="cloud" src={cloud} alt="cloud"/>
         <img className="cloud" src={cloud} alt="cloud"/>
