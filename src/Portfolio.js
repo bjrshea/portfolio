c2400f437cb5bdb6df7f8c04f5637937045a3624
Brendan Shea <bjrshea@gmail.com>
2019-01-09 21:37:47 -0800

    Add buttons to portfolio.

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -22,17 +22,44 @@ function Portfolio() {
           margin-top: 1%;
           width: 50%;
         }
-        h1, p {
-          text-align: center;
-        }
         h1 {
           font-family: 'Montserrat', sans-serif;
           margin-bottom: 3%;
+          margin-left: 5%;
+          color: #DC9E82
         }
         p {
           font-family: 'Lato', sans-serif;
-          margin: 0 5%;
+          margin-left: 5%;
+          margin-right: 8%;
           line-height: 2em;
+        }
+        button {
+          font-family: 'Montserrat', sans-serif;
+          border-radius: 5px;
+          padding: 1% 0;
+          background-color: transparent;
+          border: 1px solid #DC9E82;
+          color: #DC9E82;
+          width: 15%;
+          margin: 5% 0 0 5%;
+        }
+        button:hover {
+          cursor: pointer;
+          color: white;
+          background-color: #DC9E82;
+        }
+        button:nth-child(2) {
+          margin-left: 2%;
+        }
+        .TBA {
+          border: 1px solid #B7B5B3;
+          color: #B7B5B3;
+        }
+        .TBA:hover {
+          cursor: default;
+          color: #B7B5B3;
+          background-color: transparent;
         }
         img {
           display: block;
@@ -44,6 +71,10 @@ function Portfolio() {
         <div className="description">
           <h1>Howl</h1>
           <p>One of my first introductions to an MVC framework! Howl is your one-stop shop for finding concerts, previewing artists, and booking tickets. This app is built using Angular and leverages mutliple API calls to dynamically diplay concert details and artist's songs.</p>
+          <div className="buttons">
+            <button>VISIT GITHUB</button>
+            <button>SEE WEBSITE</button>
+          </div>
         </div>
         <div className="images">
           <a href="https://github.com/bjrshea/howl"><img src={howl} alt="Pictures of Howl"/></a>
@@ -53,6 +84,10 @@ function Portfolio() {
         <div className="description">
           <h1>Squeeze Burger</h1>
           <p>A food truck locater for the Guy Fieri approved Squeeze Burger. This app was built in React-Redux to help the owners of Squeeze Burger let their customers know where the food truck will be on a given day. This app utilizes multiple API calls, Firebase OAuth, and EmailJS for client booking requests.</p>
+          <div className="buttons">
+            <button>VISIT GITHUB</button>
+            <button className="TBA">COMING ONLINE SOON</button>
+          </div>
         </div>
         <div className="images">
           <a href="https://github.com/bjrshea/squeeze-burger"><img src={squeeze} alt="Pictures of Squeeze Burger"/></a>
@@ -62,6 +97,10 @@ function Portfolio() {
         <div className="description">
           <h1>The Remoter</h1>
           <p>An app for a pseudo co-working space that I built during my learning UX/UI in programming school. The Remoter takes you on a journey through the entire web design process — from the beginning stages of wireframing and prototyping all the way to development.</p>
+          <div className="buttons">
+            <button>VISIT GITHUB</button>
+            <button className="TBA">COMING ONLINE SOON</button>
+          </div>
         </div>
         <div className="images">
           <a href="https://github.com/bjrshea/the-remoter"><img src={remoter} alt="Pictures of The Remoter"/></a>
