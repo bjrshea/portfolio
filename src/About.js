86655f4e2f7c21a218b4af3b4ef5e11f0d1bb30f
Brendan Shea <bjrshea@gmail.com>
2019-01-12 22:02:55 -0800

    Add content on About page.

--- src/About.js
+++ src/About.js
@@ -19,24 +19,31 @@ function About() {
           margin-bottom: 5%;
           font-size: 3.5em;
         }
-        #briefcase {
+        #picture {
           display: block;
           margin-left: auto;
         	margin-right: auto;
           height: auto;
           width: 20%;
         }
+        #summary {
+          width: 50%;
+        }
+        p {
+          font-family: 'Lato', serif;
+        }
       `}</style>
       <div id="about-page-box">
         <h1 id="about-header">About Me</h1>
-        <img id="briefcase" src={about} alt="Picture of briefcase"/>
+        <img id="picture" src={about} alt="Picture of briefcase"/>
       </div>
-      <br/>
-      <p>I'm passionate about building applications that are minimal and have value for its users. I believe in the notion that less is more, and I think it’s a developer’s duty to make programs that are clear and effortless to use.</p>
-      <br/>
-      <p>JavaScript and React-Redux are my bread and butter, but I'm eager to take on projects that have me expanding my toolkit. I also love design and using CSS/Sass, responsive design, and animations to really make an app pop.</p>
-      <br/>
-      <p>In my spare time, I enjoy any activity that gets me in the mountains. Much of my inspiration comes from there.</p>
+      <div id="summary">
+        <p>We're in an age where over 55% of world's population has access to the internet (woah!). That 55% can range from your average teenager, your parents, your parent's parents, people with disabilities, etc. For that reason, I think that it's a developers duty to make websites that are easy to understand and effortless to use. I believe in a minimalistic approach not only asthetically, but also informatively.</p>
+        <br/>
+        <p>I have exposure to manyReact-Redux is my go-to tool. With that said, I'm eager to take on projects that have me expanding my toolkit. I also love design and using CSS/Sass, responsive design, and animations to really make an app pop.</p>
+        <br/>
+        <p>In my spare time, I enjoy any activity that gets me in the mountains. Much of my inspiration comes from there.</p>
+      </div>
     </div>
   );
 }
